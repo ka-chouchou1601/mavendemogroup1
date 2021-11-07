040c62cda873b0b40ca8dc77c75d640f5f72f8e1
chouella <kanyonga.chouell@gmail.com>
2021-11-07 20:41:05 +0300

    group1firstcommit

---
+++ maven-demo/src/main/java/com/mycompany/maven/demo/App.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.maven.demo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class App {
+    int sum(int a, int b) {
+        return (a + b);
+    }
+    public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    System.out.println(sum);
+    }
+}
---
+++ maven-demo/src/test/java/com/codebind/maven/demo/AppTest.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.codebind.maven.demo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AppTest {
+    
+    public AppTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
